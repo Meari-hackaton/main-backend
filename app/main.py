24b1341eefa6d8e428a1981a9b5de363c068f88e
summit1123 <ruggy245@naver.com>
2025-08-24 01:31:29 +0900

    fix: CORS 설정 수정 - withCredentials 지원을 위해 와일드카드 제거

--- app/main.py
+++ app/main.py
@@ -32,9 +32,11 @@ app.add_middleware(
         "http://localhost:8001",
         "http://ec2-43-200-4-71.ap-northeast-2.compute.amazonaws.com",  # Frontend EC2
         "http://ec2-43-200-4-71.ap-northeast-2.compute.amazonaws.com:3000",
+        "http://43.200.4.71",  # Frontend IP 직접 접근
+        "http://43.200.4.71:3000",
         "http://ec2-13-125-128-95.ap-northeast-2.compute.amazonaws.com",  # Backend EC2
         "http://ec2-13-125-128-95.ap-northeast-2.compute.amazonaws.com:8000",
-        "*"  # 개발용 (프로덕션에서는 제거)
+        # "*" 제거 - withCredentials와 충돌
     ],
     allow_credentials=True,
     allow_methods=["*"],
